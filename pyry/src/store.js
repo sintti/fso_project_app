07604e12a4765dbdedabbfbb37ad4556ca6a9068
sintti <erkka.karjalainen@gmail.com>
2020-11-12 10:06:05 +0200

    redux store fixed

--- pyry/src/store.js
+++ pyry/src/store.js
@@ -3,12 +3,10 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import clientReducer from './reducers/clientReducer'
-import userReducer from './reducers/userReducer'
 import workReducer from './reducers/workReducer'
 import notificationReducer from './reducers/notificationReducer'
 
 const reducer = combineReducers({
-  user: userReducer,
   clients: clientReducer,
   work: workReducer,
   notifications: notificationReducer
